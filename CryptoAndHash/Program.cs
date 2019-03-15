a00bd27157bbe9cf4dfa797355bba495d8bd2293
LSViana <lv201122@gmail.com>
2019-03-14 21:42:20 -0300

    Updating description of the HMAC/Keyed hash after noticing the key is actually shared between the parties

--- CryptoAndHash/Program.cs
+++ CryptoAndHash/Program.cs
@@ -6,17 +6,18 @@ namespace CryptoAndHash
     {
         static void Main(string[] args)
         {
-            // Hashes provide a single way to transform data, which can be used in password, file checksum...
-            Hash.Run();
-            Console.WriteLine();
-            // Keyed hashes provide a single way to transform data using a single secret key, which can be used to verify authenticity and data integrity. In this case, the generated hash (also called MAC) must be supplied with the original message, but the key used in generation is kept hidden, it is not useful to hide the message content.
+            /// Hashes provide a single way to transform data, which can be used in password, file checksum...
+            //Hash.Run();
+            //Console.WriteLine();
+            /// Keyed hashes provide a single way to transform data using a single secret key, which can be used to verify authenticity and data integrity. In this case, the generated hash (also called MAC) must be supplied with the original message, it is not useful to hide the message content.
             KeyedHash.Run();
             Console.WriteLine();
-            // Symmetric crypto provide two ways to transform data using a single secret key, which can be used to encrypt and decrypt data, providing a safe way between two parts to communicate. This is not useful for verify authenticity among many parts, since anyone could generate new valid messages. This is not safe because the key must be shared, if it's captured, the whole process is compromised.
-            SymmetricCrypto.Run();
-            Console.WriteLine();
-            // Asymmetric crypto [fill it here]
-            AsymmetricCrypto.Run();
+            /// Symmetric crypto provide two ways to transform data using a single secret key, which can be used to encrypt and decrypt data, providing a safe way between two parts to communicate. This is not useful for verify authenticity among many parts, since anyone could generate new valid messages. This is not safe because the key must be shared, if it's captured, the whole process is compromised.
+            //SymmetricCrypto.Run();
+            //Console.WriteLine();
+            /// Asymmetric crypto [fill it here]
+            //AsymmetricCrypto.Run();
+            //Console.WriteLine();
         }
     }
 }
