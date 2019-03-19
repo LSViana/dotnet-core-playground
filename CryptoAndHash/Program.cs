f9c42363e34b04d3d914df685044408bb21bd404
LSViana <lv201122@gmail.com>
2019-03-19 20:08:06 -0300

    Added ECDiffieHellman cryptography method mimicking sender-receiver

--- CryptoAndHash/Program.cs
+++ CryptoAndHash/Program.cs
@@ -17,7 +17,6 @@ namespace CryptoAndHash
             Console.WriteLine();
             /// Asymmetric crypto provide two ways to transform data using a pair of public and private keys, which can be used to encrypt and decrypt data, respectively (depending on the case the order changes), providing a safe way between two parts to communicate. This may be used to verify authenticity, but not to sign data between many parts. The main weakness of this process is the possibility of someone altering the public key in the middle (a MITM attack), which would give the attacker the original message, which could be encrypted with the original addressee public key and forwarded with no one noticing.
             AsymmetricCrypto.Run();
-            Console.WriteLine();
         }
     }
 }
