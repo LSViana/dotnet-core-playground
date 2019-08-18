9a64cf6fabb1769b7e5580d73b86bb77d87a0a16
LSViana <lv201122@gmail.com>
2019-08-17 23:03:59 -0300

    Added ML and StreamIO project

---
+++ StreamIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
