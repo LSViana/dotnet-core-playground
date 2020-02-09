e956b720b70fe3797ed1587fab34d69945f557fe
LSViana <lv201122@gmail.com>
2020-02-09 09:01:07 -0300

    Added GraphQL.ASP sample code to query a Controller

---
+++ GraphQL.ASP/Controllers/GraphQLController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.ASP.Models.Queries;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GraphQL.ASP.Controllers
+{
+    [Route("[controller]")]
+    public class GraphQLController : Controller
+    {
+        [HttpPost]
+        public async Task Query([FromBody] IDictionary<string, string> query)
+        {
+            var schema = new Schema { Query = new StarWarsQuery() };
+            var json = await schema.ExecuteAsync(options =>
+            {
+                string operationName;
+                options.OperationName = query.TryGetValue("operationName", out operationName) ? operationName : "";
+                options.Query = query["query"];
+            });
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            HttpContext.Response.StatusCode = 200;
+            HttpContext.Response.ContentType = "application/json";
+            HttpContext.Response.ContentLength = jsonBytes.Length;
+            var result = await HttpContext.Response.BodyWriter.WriteAsync(jsonBytes);
+            await HttpContext.Response.CompleteAsync();
+        }
+    }
+}
---
+++ GraphQL.ASP/Models/Queries/StarWarsQuery.cs
@@ -0,0 +1,38 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.ASP.Models.Queries
+{
+    public class Droid
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DroidType : ObjectGraphType<Droid>
+    {
+        public DroidType()
+        {
+            Field(x => x.Id).Description($"The {nameof(Droid.Id)} of the {nameof(Droid)}");
+            Field(x => x.Name).Description($"The {nameof(Droid.Name)} of the {nameof(Droid)}");
+        }
+    }
+
+    public class StarWarsQuery : ObjectGraphType
+    {
+        public StarWarsQuery()
+        {
+            Field<DroidType>(
+                "hero",
+                resolve: context =>
+                {
+                    // The filtering and stuff can be processed here
+                    return new Droid { Id = 1, Name = "R2-D2" };
+                }
+            );
+        }
+    }
+}
